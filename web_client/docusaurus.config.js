c69eb24fadcae26191ffdd0017278dae97cac5fa
Radi Cho <radicho123@gmail.com>
2021-02-27 23:59:04 +0200

    Update the web client template.

---
+++ web_client/docusaurus.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  title: 'GeoShare',
+  tagline: 'City traffic management and Crowd dynamics with Privacy in mind',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/icon.png',
+  organizationName: 'radi-cho',
+  projectName: 'geoshare',
+  themeConfig: {
+    navbar: {
+      title: '',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.png',
+        srcDark: 'img/logo_light.png',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Try it out',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/radi-cho/InnovaOne-HackAUBG',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Built with ❤️ by <a href="https://www.linkedin.com/in/radostin-cholakov-bb4422146/" target="_blank">Radi Cho</a>.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
