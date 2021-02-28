80471dbb34640c1822fdea60c627adc400904e6b
Radi Cho <radicho123@gmail.com>
2021-02-28 10:59:50 +0200

    Make the demo realistic and dynamic.

--- web_client/static/app/index.js
+++ web_client/static/app/index.js
@@ -38,10 +38,12 @@ function initMapDemo() {
 
     heatmap.setMap(map);
     cleanUI();
+    covidChecks();
+    trafficLevels(true);
 }
 
 function loadHeatMap() {
-    while(heatmapData.getLength() > 0) heatmapData.pop();
+    while (heatmapData.getLength() > 0) heatmapData.pop();
     demoData.forEach(loc => {
         heatmapData.push({ location: new google.maps.LatLng(loc[0], loc[1]), weight: 0.07 });
     });
@@ -51,7 +53,7 @@ function loadHeatMap() {
 
 function initMapFirebase() {
     const coords = [41.6229474, 24.1651491];
-    const dospat = new google.maps.LatLng(coords[0],coords[1]);
+    const dospat = new google.maps.LatLng(coords[0], coords[1]);
     map = new google.maps.Map(document.getElementById('map'), {
         center: dospat,
         zoom: 13,
@@ -66,7 +68,7 @@ function initMapFirebase() {
     const geoDataRef = database.ref('geo/');
     geoDataRef.on('value', (snapshot) => {
         const data = snapshot.val();
-        while(heatmapDataFirebase.getLength() > 0) heatmapData.pop();
+        while (heatmapDataFirebase.getLength() > 0) heatmapData.pop();
         Object.keys(data).forEach((geo) => {
             heatmapDataFirebase.push({ location: new google.maps.LatLng(data[geo].lat, data[geo].lng), weight: 0.9 });
         })
@@ -76,7 +78,7 @@ function initMapFirebase() {
 }
 
 function initMap() {
-    const world = new google.maps.LatLng(35,-30);
+    const world = new google.maps.LatLng(35, -30);
     map = new google.maps.Map(document.getElementById('map'), {
         center: world,
         zoom: 3,
@@ -84,4 +86,50 @@ function initMap() {
     });
 
     cleanUI();
+}
+
+function countRect(target) {
+    return demoData.filter(el => {
+        return el[1] > target[0][1] && el[1] < target[1][1] && el[0] < target[1][0] && el[0] > target[2][0];
+    }).length;
+}
+
+function covidChecks() {
+    const targetName = "Denny Park";
+    const acceptable = 215;
+    const current = countRect(dennyPark);
+
+    document.querySelector("#covid-target-name").innerHTML = targetName;
+    document.querySelector("#covid-acceptable").innerHTML = acceptable;
+    document.querySelector("#covid-current").innerHTML = current;
+
+    const statusDOM = document.querySelector("#covid-status");
+    if (current > acceptable) statusDOM.innerHTML = "too crowded";
+    if (current <= acceptable && current > acceptable * 0.7) statusDOM.innerHTML = "mildly crowded";
+    if (current < acceptable && current <= acceptable * 0.7) statusDOM.innerHTML = "not too crowded";
+}
+
+function trafficLevels(updateDOM) {
+    const targets = [
+        { name: "9th Ave N", data: ave9 },
+        { name: "Denny Way Str", data: dennyWay }
+    ];
+
+    const acceptable = [100, 100];
+    const currentLevels = targets.map(t => countRect(t.data));
+
+    if (updateDOM) {
+        const lines = currentLevels.map((lvl, i) => {
+            if (lvl > acceptable[i]) return 'Traffic levels for "' + targets[i].name + '": ' + "<b>High</b>";
+            if (lvl <= acceptable[i] && lvl > acceptable[i] * 0.7) return 'Traffic levels for "' + targets[i].name + '": ' + "<b>Mild</b>";
+            if (lvl < acceptable[i] && lvl <= acceptable[i] * 0.7) return 'Traffic levels for "' + targets[i].name + '": ' + "<b>Low</b>";
+        });
+
+        let resultElements = "";
+        lines.forEach(line => resultElements += "<li>" + line + "</li>");
+
+        document.getElementById("traffic-list").innerHTML = resultElements;
+    }
+
+    return currentLevels;
 }
